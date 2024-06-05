018deca4a774f1346c5d315401df98bb57fec383
Naim21 <2002natorres@gmail.com>
2024-06-05 15:03:22 -0300

    a

--- TP3/src/tests/CliqueMayorCantVerticesTest.java
+++ TP3/src/tests/CliqueMayorCantVerticesTest.java
@@ -63,16 +63,22 @@ public class CliqueMayorCantVerticesTest {
 		
 	}
 	
+	@Test
+	public void vacioTest() {
+		Grafo grafo = new Grafo();
+		
+		EncontrarCliqueMayorCantVertices obtenido = new EncontrarCliqueMayorCantVertices();
+		List<Vertice> cliqueObtenida = obtenido.encontrarCliqueMayorCantVertices(grafo);
+		
+		assertEquals(0, cliqueObtenida.size());
+	}
 	
-
 	private List<Vertice> cliquePrueba() {
 		List<Vertice> clique = new ArrayList<Vertice>();
 		clique.add(new Vertice(0,1));
 		clique.add(new Vertice(1,1));
 		clique.add(new Vertice(2,2));
 		clique.add(new Vertice(3,3));
-		clique.add(new Vertice(4,4));
-		clique.add(new Vertice(5,5));
 		return clique;
 	}
 
@@ -84,17 +90,10 @@ public class CliqueMayorCantVerticesTest {
 		grafo.agregarVertice(3, 3);
 		grafo.agregarVertice(4, 4);
 		grafo.agregarVertice(5, 5);
-		grafo.agregarVertice(6, 10);
-		grafo.agregarVertice(7, 10);
-		grafo.agregarVertice(8, 10);
-		grafo.agregarVertice(9, 10);
-//		grafo.inicializarMatrizAdy();
 		
 		grafo.agregarArista(0, 1);
 		grafo.agregarArista(0, 2);
 		grafo.agregarArista(0, 3);
-		grafo.agregarArista(0, 4);
-		grafo.agregarArista(0, 5);
 		
 		grafo.agregarArista(1, 2);
 		grafo.agregarArista(1, 3);
@@ -102,17 +101,9 @@ public class CliqueMayorCantVerticesTest {
 		grafo.agregarArista(1, 5);
 		
 		grafo.agregarArista(2, 3);
-		grafo.agregarArista(2, 4);
-		grafo.agregarArista(2, 5);
-		
-		grafo.agregarArista(3, 4);
-		grafo.agregarArista(3, 5);
 		
 		grafo.agregarArista(4, 5);
-		
-		grafo.agregarArista(9, 8);
-		grafo.agregarArista(9, 7);
-		grafo.agregarArista(8, 7);
+
 		return grafo;
 	}
 	
@@ -122,13 +113,7 @@ public class CliqueMayorCantVerticesTest {
 		clique.add(new Vertice(1,1));
 		clique.add(new Vertice(2,2));
 		clique.add(new Vertice(3,3));
-		clique.add(new Vertice(4,4));
-		
-//		clique.add(new Vertice(5,10));
-//		clique.add(new Vertice(6,10));
-//		clique.add(new Vertice(7,10));
-//		clique.add(new Vertice(8,10));
-//		clique.add(new Vertice(9,10));
+
 		return clique;
 	}
 	
@@ -139,41 +124,24 @@ public class CliqueMayorCantVerticesTest {
 		grafo.agregarVertice(2, 2);
 		grafo.agregarVertice(3, 3);
 		grafo.agregarVertice(4, 4);
-		grafo.agregarVertice(5, 10);
-		grafo.agregarVertice(6, 10);
-		grafo.agregarVertice(7, 10);
-		grafo.agregarVertice(8, 10);
-		grafo.agregarVertice(9, 10);
-//		grafo.inicializarMatrizAdy();
+		grafo.agregarVertice(5, 5);
 		
 		grafo.agregarArista(0, 1);
 		grafo.agregarArista(0, 2);
 		grafo.agregarArista(0, 3);
-		grafo.agregarArista(0, 4);
 		
 		grafo.agregarArista(1, 2);
 		grafo.agregarArista(1, 3);
 		grafo.agregarArista(1, 4);
+		grafo.agregarArista(1, 5);
 		
 		grafo.agregarArista(2, 3);
 		grafo.agregarArista(2, 4);
+		grafo.agregarArista(2, 5);
 		
-		grafo.agregarArista(3, 4);
-				
-		grafo.agregarArista(5, 6);
-		grafo.agregarArista(5, 7);
-		grafo.agregarArista(5, 8);
-		grafo.agregarArista(5, 9);
+		grafo.agregarArista(4, 5);
 		
-		grafo.agregarArista(6, 7);
-		grafo.agregarArista(6, 8);
-		grafo.agregarArista(6, 9);
-		
-		grafo.agregarArista(7, 8);
-		grafo.agregarArista(7, 9);
-		
-		grafo.agregarArista(8, 9);
 		return grafo;
 	}
-
+	
 }
--- TP3/src/tests/EncontrarCliqueMayorPesoTest.java
+++ TP3/src/tests/EncontrarCliqueMayorPesoTest.java
@@ -82,4 +82,53 @@ public class EncontrarCliqueMayorPesoTest {
         assertEquals(1, cliqueMax.size());
         assertTrue(cliqueMax.contains(grafo.getVertice(0)));
     }
+    
+    @Test
+    public void mismoPesoTotalDistintosVerticesTest() {
+    	grafoPrueba();
+    	
+    	EncontrarCliqueMayorPesoPorPeso obtenido = new EncontrarCliqueMayorPesoPorPeso();
+    	List<Vertice> cliqueObtenida = EncontrarCliqueMayorPesoPorPeso.encontrarCliqueDeMayorPeso(grafo);
+    	
+    	List<Integer> cliqueEsperada = cliquePrueba();
+    	
+    	for (Vertice v : cliqueObtenida) {
+    		assertTrue(cliqueEsperada.contains(v.getId()));
+    	}
+    }
+
+	private void grafoPrueba() {
+		grafo.agregarVertice(0, 2.0);
+    	grafo.agregarVertice(1, 1.5);
+    	grafo.agregarVertice(2, 3.0);
+    	grafo.agregarVertice(3, 2.5);
+    	grafo.agregarVertice(4, 1.0);
+    	grafo.agregarVertice(5, 1.0);
+    	
+    	grafo.agregarArista(0, 1);
+    	grafo.agregarArista(0, 2);
+    	grafo.agregarArista(0, 3);
+    	grafo.agregarArista(1, 2);
+    	grafo.agregarArista(2, 3);
+    	grafo.agregarArista(2, 4);
+    	grafo.agregarArista(2, 5);
+    	grafo.agregarArista(3, 4);
+    	grafo.agregarArista(3, 5);
+    	grafo.agregarArista(4, 5);
+	}
+
+	private List<Integer> cliquePrueba() {
+		List<Integer> cliqueEsperada = new ArrayList<Integer>();
+		
+//    	cliqueEsperada.add(2);
+//    	cliqueEsperada.add(3);
+//    	cliqueEsperada.add(4);
+//    	cliqueEsperada.add(5);
+    	
+    	cliqueEsperada.add(0);
+    	cliqueEsperada.add(2);
+    	cliqueEsperada.add(3);
+		return cliqueEsperada;
+	}
+    
 }
